99885d9fe2c959e45e03af465ea421bc591e432f
Edgar Blancas Morales <edgar@MacBook-Air-de-Edgar.local>
2021-06-02 17:50:44 -0500

    JWT implementado

--- models/server.js
+++ models/server.js
@@ -7,7 +7,9 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT
+
         this.usuariosPath = '/api/usuarios';
+        this.authPath = '/api/auth';
 
         // Conexión a la base de datos
         this.connectarDB();
@@ -24,6 +26,7 @@ class Server {
     }
 
     routes() {
+        this.app.use(this.authPath, require('../routes/auth'));
         this.app.use(this.usuariosPath, require('../routes/usuarios'));
     }
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,7 +3,12 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { esRolValido, correoExiste, existeUsuarioPorId } = require('../helpers/db-validators');
-const { validarCampos } = require('../middlewares/validar-campos');
+
+// const { validarCampos } = require('../middlewares/validar-campos');
+// const { validarJWT } = require('../middlewares/validar-jwt');
+// const { esAdminRole, tieneRol } = require('../middlewares/validar-roles');
+
+const { validarCampos, validarJWT, esAdminRole, tieneRol} = require('../middlewares');
 
 const { usuarioGet, usuarioPost, usuarioPut, usuarioDelete } = require('../controllers/usuarios');
 
@@ -29,6 +34,9 @@ router.put('/:id', [
 ], usuarioPut);
 
 router.delete('/:id',[
+    validarJWT,
+    // esAdminRole,
+    tieneRol('ADMIN_ROLE', 'VENTAS_ROLE'),
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom( existeUsuarioPorId ),
     validarCampos
